3b1a03a9e582ca5b8340ddc5b177bda2f8424b2b
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-12-02 14:10:42 +0400

    added sarcasm

---
+++ Commands/Sarcasm.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace utilities_cs
+{
+    public class Sarcasm
+    {
+        public static void Sarcasm_(string[] args)
+        {
+            string text = string.Join(" ", args[1..]);
+            List<string> converted = new();
+            char current_case = 'u';
+            foreach (char i in text)
+            {
+                string i_str = i.ToString();
+                if (current_case == 'u') {
+                    converted.Add(i_str.ToUpper());
+                    current_case = 'l';
+                }
+                else if (current_case == 'l') {
+                    converted.Add(i_str.ToLower());
+                    current_case = 'u';
+                }
+            }
+            string sarcasm_text = string.Join("", converted);
+            WindowsClipboard.SetText(sarcasm_text);
+            Utils.Notification("Success!", "Message copied to clipboard.", 3);
+        }
+    }
+}
