5d07863c07b0dd6afb5cd29f365be78a06278127
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-12-02 14:11:05 +0400

    added several new commands

--- Utilities.cs
+++ Utilities.cs
@@ -5,20 +5,22 @@ namespace utilities_cs
 {
     class Program
     {
-        static Dictionary<string, Action<string>> commands = new()
+        static Dictionary<string, Action<string[]>> commands = new()
         {
-            { "cursive", Cursive.cursive }
+            { "cursive", Cursive.cursive }, { "copypaste", Copypaste.cp }, { "cp", Copypaste.cp },
+            { "upper", Upper.Uppercase }, { "lower", Lower.Lowercase }, { "sarcasm", Sarcasm.Sarcasm_ }
         };
 
         static void Main(string[] args)
         {
-            var cmd = args[0];
+            var cmd = args[0].ToLower();
             var f = commands.GetValueOrDefault(cmd, (args) => Utils.Notification(
                     "Welp.",
-                    "It seems utilities couldn't understand what command you were trying to use."
+                    "It seems utilities couldn't understand what command you were trying to use.",
+                    6
                 )
             );
-            f.Invoke(string.Join(" ", args[1..]));
+            f.Invoke(args);
         }
 
     }
