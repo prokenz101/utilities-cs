726bf55f4795b7f9683b262209cea6f014ce0cde
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-12-03 12:22:03 +0400

    added indextest to lower

--- Commands/Lower.cs
+++ Commands/Lower.cs
@@ -1,6 +1,9 @@
 namespace utilities_cs {
     public class Lower {
         public static void Lowercase(string[] args) {
+            if (Utils.IndexTest(args, "Huh.", "It seems you did not input any text for lowercase.", 4)) {
+                return;
+            }
             string text = string.Join(" ", args[1..]);
             string lower_text = text.ToLower();
             WindowsClipboard.SetText(lower_text);
