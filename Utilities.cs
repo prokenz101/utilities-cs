6e45da8f8a71879402d173b4739d6b718dcc378c
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-12-03 12:23:07 +0400

    made it warn you if two utilities-cses are open

--- Utilities.cs
+++ Utilities.cs
@@ -4,20 +4,16 @@ using System.Windows.Forms;
 using System.Drawing;
 using System.Reflection;
 
-namespace utilities_cs
-{
-    class Program
-    {
+namespace utilities_cs {
+    class Program {
         [STAThread]
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             Application.EnableVisualStyles();
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
 
             var app = new UtilitiesAppContext();
-            Application.ApplicationExit += delegate
-            {
+            Application.ApplicationExit += delegate {
                 app.Exit();
             };
 
@@ -25,19 +21,30 @@ namespace utilities_cs
         }
 
     }
-    public class UtilitiesAppContext : ApplicationContext
-    {
+    public class UtilitiesAppContext : ApplicationContext {
         public static Dictionary<string, Action<string[]>> commands = new()
         {
             { "cursive", Cursive.cursive },
+            { "sarcasm", Sarcasm.Sarcasm_ },
             { "copypaste", Copypaste.cp },
             { "cp", Copypaste.cp },
             { "upper", Upper.Uppercase },
+            { "uppercase", Upper.Uppercase },
             { "lower", Lower.Lowercase },
-            { "sarcasm", Sarcasm.Sarcasm_ }
+            { "lowercase", Lower.Lowercase },
+            { "bubbletext", Bubble.bubbletext },
+            { "bubble", Bubble.bubbletext },
+            { "doublestruck", Doublestruck.dbs },
+            { "dbs", Doublestruck.dbs },
+            { "-", BrowserSearch.GoogleSearch },
+            { "youtube", BrowserSearch.YouTubeSearch },
+            { "yt", BrowserSearch.YouTubeSearch },
+            { "images", BrowserSearch.ImageSearch },
+            { "spacer", Spacer.spacer },
+            { "spoilerspam", Spoilerspam.spoilerspam },
+            { "reverse", Reverse.reverse }
         };
-        public static void Utilities(string[] args)
-        {
+        public static void Utilities(string[] args) {
             var cmd = args[0].ToLower();
             var f = commands.GetValueOrDefault(cmd, (args) => Utils.Notification(
                     "Welp.",
@@ -50,27 +57,32 @@ namespace utilities_cs
         private NotifyIcon trayIcon;
         private KeyboardHook hook;
 
-        public UtilitiesAppContext()
-        {
+        public UtilitiesAppContext() {
             hook = new KeyboardHook();
-            hook.KeyPressed += delegate
-            {
+            hook.KeyPressed += delegate {
                 SendKeys.SendWait("^a");
                 SendKeys.Send("^c");
                 SendKeys.Send("{ESC}");
                 string[] args = Clipboard.GetText().Split(" ");
                 Utilities(args);
             };
-            hook.RegisterHotKey(ModifierKeys.Control, Keys.F9);
-
-            trayIcon = new NotifyIcon()
-            {
+            try {
+                hook.RegisterHotKey(ModifierKeys.Control, Keys.F9);
+            } catch (InvalidOperationException) {
+                Utils.Notification(
+                    "Something went wrong.",
+                    @"utilities-cs was unable to register a hotkey.
+This could be because you have multiple verions of the application running.",
+                    6
+                );
+                Exit();
+            }
+            trayIcon = new NotifyIcon() {
                 Text = "utilities-cs"
             };
             var menu = new ContextMenuStrip();
 
-            menu.Items.Add("Help Center", null, delegate
-            {
+            menu.Items.Add("Help Center", null, delegate {
                 Utils.Notification("lmfao", "you thought there was a helpcenter?", 3);
             }
             );
@@ -81,9 +93,9 @@ namespace utilities_cs
             trayIcon.Visible = true;
         }
 
-        public void Exit()
-        {
+        public void Exit() {
             trayIcon.Visible = false;
+            hook.Dispose();
             Application.Exit();
         }
     }
