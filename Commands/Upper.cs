ec65018361b02523d26287a2cbf48a32ef3a9a5c
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-12-03 12:22:40 +0400

    added indextest to upper

--- Commands/Upper.cs
+++ Commands/Upper.cs
@@ -1,6 +1,9 @@
 namespace utilities_cs {
     public class Upper {
         public static void Uppercase(string[] args) {
+            if (Utils.IndexTest(args, "Huh.", "It seems you did not input any text for uppercase.", 4)) {
+                return;
+            }
             string text = string.Join(" ", args[1..]);
             string upper_text = text.ToUpper();
             WindowsClipboard.SetText(upper_text);
