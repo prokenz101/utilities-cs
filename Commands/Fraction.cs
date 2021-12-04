20b3f0e791c8e4e8205347794d56de34e8feaec9
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-12-04 22:01:49 +0400

    made fraction

---
+++ Commands/Fraction.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+
+namespace utilities_cs {
+    public class Fraction {
+        public static void fraction(string[] args) {
+            if (Utils.IndexTest(args, "Huh.", "It seems you did not input a fraction.", 3)) {
+                return;
+            }
+            string text = string.Join(" ", args[1..]);
+            List<string> converted = new();
+            Dictionary<char, string[]> fraction_dict = new Dictionary<char, string[]> {
+                { '0', new string[]{"⁰", "₀"}},
+                { '1', new string[]{"¹", "₁"}},
+                { '2', new string[]{"²", "₂"}},
+                { '3', new string[]{"³", "₃"}},
+                { '4', new string[]{"⁴", "₄"}},
+                { '5', new string[]{"⁵", "₅"}},
+                { '6', new string[]{"⁶", "₆"}},
+                { '7', new string[]{"⁷", "₇"}},
+                { '8', new string[]{"⁸", "₈"}},
+                { '9', new string[]{"⁹", "₉"}},
+                { '=', new string[]{"⁼", "₌"}},
+                { '+', new string[]{"⁺", "₊"}},
+                { '-', new string[]{"⁻", "₋"}},
+                { '(', new string[]{"⁽", "₍"}},
+                { ')', new string[]{"⁾", "₎"}},
+                { 'a', new string[]{"ᵃ", "ₐ"}},
+                { 'b', new string[]{"ᵇ", "failed"}},
+                { 'c', new string[]{"ᶜ", "failed"}},
+                { 'd', new string[]{"ᵈ", "failed"}},
+                { 'e', new string[]{"ᵉ", "ₑ"}},
+                { 'f', new string[]{"ᶠ", "failed"}},
+                { 'g', new string[]{"ᵍ", "failed"}},
+                { 'h', new string[]{"ʰ", "ₕ"}},
+                { 'i', new string[]{"ⁱ", "ᵢ"}},
+                { 'j', new string[]{"ʲ", "ⱼ"}},
+                { 'k', new string[]{"ᵏ", "ₖ"}},
+                { 'l', new string[]{"ˡ", "ₗ"}},
+                { 'm', new string[]{"ᵐ", "ₘ"}},
+                { 'n', new string[]{"ⁿ", "ₙ"}},
+                { 'o', new string[]{"ᵒ", "ₒ"}},
+                { 'p', new string[]{"ᵖ", "ₚ"}},
+                { 'r', new string[]{"ʳ", "ᵣ"}},
+                { 's', new string[]{"ˢ", "ₛ"}},
+                { 't', new string[]{"ᵗ", "ₜ"}},
+                { 'u', new string[]{"ᵘ", "ᵤ"}},
+                { 'v', new string[]{"ᵛ", "ᵥ"}},
+                { 'w', new string[]{"ʷ", "failed"}},
+                { 'x', new string[]{"ˣ", "ₓ"}},
+                { 'y', new string[]{"ʸ", "failed"}},
+                { 'z', new string[]{"ᶻ", "failed"}},
+            };
+
+            string[] slash_split = text.Split("/");
+            string numerator = slash_split[0];
+            string denominator = slash_split[1];
+
+            foreach (char x in numerator) {
+                if (fraction_dict.ContainsKey(x)) {
+                    string i = fraction_dict[x][0];
+                    converted.Add(i);
+                } else {
+                    Utils.Notification(
+                        "Something went wrong.",
+                        "Either the code is broken, or you did not input the parameters correctly.",
+                        4
+                    );
+                    return;
+                }
+            }
+
+            converted.Add("⁄");
+
+            foreach (char x in denominator) {
+                if (fraction_dict.ContainsKey(x)) {
+                    string i = fraction_dict[x][1];
+                    if (i != "failed") {
+                        converted.Add(i);
+                    } else {
+                        Utils.Notification("Hey!",
+                            @"It seems you tried to input a character that's not supported.",
+                            7
+                        );
+                        return;
+                    }
+                } else {
+                    Utils.Notification(
+                        "Something went wrong.",
+                        "Either the code is broken, or you did not input the parameters correctly.",
+                        4
+                    );
+                    return;
+                }
+            }
+
+            string fraction = string.Join("", converted);
+            WindowsClipboard.SetText(fraction);
+            Utils.Notification("Success!", "Message copied to clipboard.");
+        }
+    }
+}
