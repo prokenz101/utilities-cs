add509c22cf47a781a2e6f77c35aa12dd6dd9184
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-12-04 17:17:24 +0400

    made HookManager to handle all keyboardhooks

---
+++ UtilsHookManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace utilities_cs {
+    public class HookExistsException : Exception { }
+    public class HookManager {
+        static Dictionary<string, KeyboardHook> keyboardHooks = new();
+
+        public static void AddHook(string id, KeyboardHook hook) {
+            if (!keyboardHooks.ContainsKey(id)) {
+                keyboardHooks.Add(id, hook);
+            } else { throw new HookExistsException(); }
+        }
+        public static void AddHook(string id, ModifierKeys modifiers, Keys keys, Action onPressed, Action onFail = null) {
+            KeyboardHook hook = new();
+            hook.KeyPressed += delegate {
+                onPressed();
+            };
+            try {
+                hook.RegisterHotKey(modifiers, keys);
+            } catch (InvalidOperationException) {
+                onFail?.Invoke();
+                return;
+            }
+            AddHook(id, hook);
+        }
+
+        public static void UnregisterAllHooks() {
+            foreach (KeyboardHook hook in keyboardHooks.Values) {
+                hook.Dispose();
+            }
+            keyboardHooks.Clear();
+        }
+
+        public static void UnregisterHook(string id) {
+            keyboardHooks[id].Dispose();
+            keyboardHooks.Remove(id);
+        }
+    }
+}
