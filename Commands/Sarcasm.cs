ce9692e936423919299dce54fb78e3e04b8e059f
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-12-04 17:16:22 +0400

    formatted

--- Commands/Sarcasm.cs
+++ Commands/Sarcasm.cs
@@ -1,25 +1,20 @@
 using System.Collections.Generic;
 
-namespace utilities_cs
-{
-    public class Sarcasm
-    {
-        public static void Sarcasm_(string[] args)
-        {
+namespace utilities_cs {
+    public class Sarcasm {
+        public static void Sarcasm_(string[] args) {
             if (Utils.IndexTest(args, "Huh.", "It seems you did not input anything for sarcasm to 'sarcasm-ize'.", 4)) {
                 return;
             }
             string text = string.Join(" ", args[1..]);
             List<string> converted = new();
             char current_case = 'u';
-            foreach (char i in text)
-            {
+            foreach (char i in text) {
                 string i_str = i.ToString();
                 if (current_case == 'u') {
                     converted.Add(i_str.ToUpper());
                     current_case = 'l';
-                }
-                else if (current_case == 'l') {
+                } else if (current_case == 'l') {
                     converted.Add(i_str.ToLower());
                     current_case = 'u';
                 }
