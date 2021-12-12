e906c4c13efc5f82ea0d699cf31663baa39bb299
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-12-12 14:47:27 +0400

    nullable + formatted

--- UtilsKeyboardHook.cs
+++ UtilsKeyboardHook.cs
@@ -2,8 +2,8 @@ using System;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
-public sealed class KeyboardHook : IDisposable
-{
+#nullable disable
+public sealed class KeyboardHook : IDisposable {
     // Registers a hot key with Windows.
     [DllImport("user32.dll")]
     private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
@@ -14,12 +14,10 @@ public sealed class KeyboardHook : IDisposable
     /// <summary>
     /// Represents the window that is used internally to get the messages.
     /// </summary>
-    private class Window : NativeWindow, IDisposable
-    {
+    private class Window : NativeWindow, IDisposable {
         private static int WM_HOTKEY = 0x0312;
 
-        public Window()
-        {
+        public Window() {
             // create the handle for the window.
             this.CreateHandle(new CreateParams());
         }
@@ -28,13 +26,11 @@ public sealed class KeyboardHook : IDisposable
         /// Overridden to get the notifications.
         /// </summary>
         /// <param name="m"></param>
-        protected override void WndProc(ref Message m)
-        {
+        protected override void WndProc(ref Message m) {
             base.WndProc(ref m);
 
             // check if we got a hot key pressed.
-            if (m.Msg == WM_HOTKEY)
-            {
+            if (m.Msg == WM_HOTKEY) {
                 // get the keys.
                 Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
                 ModifierKeys modifier = (ModifierKeys)((int)m.LParam & 0xFFFF);
@@ -49,8 +45,7 @@ public sealed class KeyboardHook : IDisposable
 
         #region IDisposable Members
 
-        public void Dispose()
-        {
+        public void Dispose() {
             this.DestroyHandle();
         }
 
@@ -60,11 +55,9 @@ public sealed class KeyboardHook : IDisposable
     private Window _window = new Window();
     private int _currentId;
 
-    public KeyboardHook()
-    {
+    public KeyboardHook() {
         // register the event of the inner native window.
-        _window.KeyPressed += delegate (object sender, KeyPressedEventArgs args)
-        {
+        _window.KeyPressed += delegate (object sender, KeyPressedEventArgs args) {
             if (KeyPressed != null)
                 KeyPressed(this, args);
         };
@@ -75,8 +68,7 @@ public sealed class KeyboardHook : IDisposable
     /// </summary>
     /// <param name="modifier">The modifiers that are associated with the hot key.</param>
     /// <param name="key">The key itself that is associated with the hot key.</param>
-    public void RegisterHotKey(ModifierKeys modifier, Keys key)
-    {
+    public void RegisterHotKey(ModifierKeys modifier, Keys key) {
         // increment the counter.
         _currentId = _currentId + 1;
 
@@ -92,11 +84,9 @@ public sealed class KeyboardHook : IDisposable
 
     #region IDisposable Members
 
-    public void Dispose()
-    {
+    public void Dispose() {
         // unregister all the registered hot keys.
-        for (int i = _currentId; i > 0; i--)
-        {
+        for (int i = _currentId; i > 0; i--) {
             UnregisterHotKey(_window.Handle, i);
         }
 
@@ -110,24 +100,20 @@ public sealed class KeyboardHook : IDisposable
 /// <summary>
 /// Event Args for the event that is fired after the hot key has been pressed.
 /// </summary>
-public class KeyPressedEventArgs : EventArgs
-{
+public class KeyPressedEventArgs : EventArgs {
     private ModifierKeys _modifier;
     private Keys _key;
 
-    internal KeyPressedEventArgs(ModifierKeys modifier, Keys key)
-    {
+    internal KeyPressedEventArgs(ModifierKeys modifier, Keys key) {
         _modifier = modifier;
         _key = key;
     }
 
-    public ModifierKeys Modifier
-    {
+    public ModifierKeys Modifier {
         get { return _modifier; }
     }
 
-    public Keys Key
-    {
+    public Keys Key {
         get { return _key; }
     }
 }
@@ -136,8 +122,7 @@ public class KeyPressedEventArgs : EventArgs
 /// The enumeration of possible modifiers.
 /// </summary>
 [Flags]
-public enum ModifierKeys : uint
-{
+public enum ModifierKeys : uint {
     Alt = 1,
     Control = 2,
     Shift = 4,
