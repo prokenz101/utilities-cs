3a648ff6045b45b8f5e3a163f48349148450386f
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-01-14 19:21:31 +0400

    HCF now supports an infinite amount of numbers

--- Commands/HCF.cs
+++ Commands/HCF.cs
@@ -1,14 +1,30 @@
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
 namespace utilities_cs {
     public class HCF {
-        public static int hcf_main(int a, int b) {
-            while (b != 0) {
-                int temp = b;
-                b = a % b;
-                a = temp;
-            }
-            return a;
+        static BigInteger hcf(BigInteger a, BigInteger b) {
+            if (a == 0)
+                return b;
+            return hcf(b % a, a);
         }
-        public static string? GCD(string[] args, bool copy, bool notif) {
+
+        // Function to find gcd of 
+        // array of numbers
+        static BigInteger findGCD(BigInteger[] arr, BigInteger n) {
+            BigInteger result = arr[0];
+            for (int i = 1; i < n; i++) {
+                result = hcf(arr[i], result);
+
+                if (result == 1) {
+                    return 1;
+                }
+            }
+
+            return result;
+        }
+        public static string? hcf_main(string[] args, bool copy, bool notif) {
             if (Utils.IndexTest(
                     args,
                     "Huh.",
@@ -18,12 +34,40 @@ namespace utilities_cs {
             ) {
                 return null;
             }
-            int num1 = int.Parse(args[1]);
-            int num2 = int.Parse(args[2]);
-            int answer = hcf_main(num1, num2);
-            Utils.CopyCheck(copy, answer.ToString());
-            Utils.NotifCheck(notif, new string[] { "Success!", $"The answer was {answer}.", "5" });
-            return answer.ToString();
+
+            string text = string.Join(" ", args);
+
+            string regex_exp = @"(\d+)+";
+            Regex re = new Regex(regex_exp);
+            MatchCollection matches = re.Matches(text);
+            List<BigInteger> nums = new();
+
+# nullable disable
+
+            if (matches.Count >= 2) {
+                foreach (Match match in matches) {
+                    nums.Add(BigInteger.Parse(match.ToString()));
+                }
+            }
+
+            BigInteger[] nums_array = nums.ToArray();
+
+# nullable enable
+
+            try {
+                BigInteger answer = findGCD(nums_array, nums_array.Length);
+                Utils.CopyCheck(copy, answer.ToString());
+                Utils.NotifCheck(notif, new string[] { "Success!", $"The answer was {answer}.", "5" });
+                return answer.ToString();
+            } catch {
+                Utils.Notification(
+                    "Huh.",
+                    "It seems you did not input the numbers properly. Try 'hcf 15 70' as an example.",
+                    8
+                );
+
+                return null;
+            }
         }
     }
 }
