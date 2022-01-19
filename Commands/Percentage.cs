d22c8c8c285fd5f62f0c9876d62d8f385cf0511d
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-01-19 10:01:00 +0400

    used RegexFind() instead of old Regex

--- Commands/Percentage.cs
+++ Commands/Percentage.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace utilities_cs {
     public class Percentage {
         public static string? Percent(string[] args, bool copy, bool notif) {
-            if (Utils.IndexTest(args, "Huh.", "It seems you did not input anything at all.", 3)) {
+            if (Utils.IndexTest(args)) {
                 return null;
             }
             string text = string.Join(" ", args[1..]);
@@ -12,29 +13,35 @@ namespace utilities_cs {
             string regex_exp = @"(?<percent>\d+.?\d+)% of (?<number>\d+.?\d+)";
             Regex re = new Regex(regex_exp, RegexOptions.Compiled);
 
-            if (re.IsMatch(text)) {
-                // getting all values using regex
-                MatchCollection matches = re.Matches(text);
-                Match match = matches[0];
-                GroupCollection groups = match.Groups;
+            var matchToGroups = Utils.RegexFind(
+                text,
+                @"(?<percent>\d+.?\d+)% of (?<number>\d+.?\d+)",
+                useIsMatch: true,
+                () => {
+                    Utils.Notification(
+                        "Huh.",
+                        "It seems you did not input the parameters correctly. Try '% 50% of 300'."
+                    );
+                }
+            );
 
-                float p = float.Parse(groups["percent"].Value) / 100; // percentage in decimal
-                float x = float.Parse(groups["number"].Value); // number
-                float y = p * x; // answer
+# nullable disable
+
+            List<float> nums = new();
+
+            foreach (Dictionary<Match, GroupCollection> dict in matchToGroups) {
+                foreach (KeyValuePair<Match, GroupCollection> kvp in dict) {
+                    nums.Add(float.Parse(kvp.Value["percent"].ToString()) / 100); // percentage in decimal
+                    nums.Add(float.Parse(kvp.Value["number"].ToString())); // number
+                }
+            }
+
+                float y = nums[0] * nums[1]; // answer
 
                 Utils.NotifCheck(notif, new string[] { "Success!", $"The Answer is {y}.", "5" });
                 Utils.CopyCheck(copy, y.ToString());
                 return y.ToString();
 
-            } else {
-                Utils.Notification(
-                    "Huh.",
-                    "It seems you did not input the parameters for percentage properly. Try '% 50% of 300'.",
-                    4
-                );
-
-                return null;
-            }
         }
     }
 }
