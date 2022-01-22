4e6d8970e270b7b9533d0f4cfe795282609aff35
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-01-22 13:50:34 +0400

    formatted and name change

--- Commands/Help.cs
+++ Commands/Help.cs
@@ -2,12 +2,12 @@ using System.Diagnostics;
 
 namespace utilities_cs {
     public class Help {
-        public static void help(string[] args) {
+        public static void Wiki(string[] args) {
             Process.Start(
                 new ProcessStartInfo(
-                        "cmd",
-                        $"/c start https://github.com/prokenz101/utilities-py/wiki/Utilities-Wiki-(Windows,-C%23-and-Python)"
-                    ) { CreateNoWindow = true }
+                    "cmd",
+                    $"/c start https://github.com/prokenz101/utilities-py/wiki/Utilities-Wiki-(Windows,-C%23-and-Python)"
+                ) { CreateNoWindow = true }
             );
         }
     }
