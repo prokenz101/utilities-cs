0737b03f7f2f1fa955b78b287b6e54db81ab86d1
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-04-15 09:32:29 +0400

    formatted

--- UtilsCommands.cs
+++ UtilsCommands.cs
@@ -289,20 +289,15 @@ namespace utilities_cs {
 
                             Utils.CopyCheck(true, v.ToString());
                             Utils.NotifCheck(
-                                true, new string[] { v.ToString(), $"The factorial is: {v.ToString()}", "5" }
-                            );
-                            return v.ToString();
+                                true,
+                                new string[] { v.ToString(), $"The factorial is: {v.ToString()}", "5" }
+                            ); return v.ToString();
                         });
                     } catch {
                         Utils.NotifCheck(
                             true,
-                            new string[] {
-                        "Huh.",
-                        "It seems that the number you inputted was not a number.",
-                        "4"
-                            }
-                        );
-                        return;
+                            new string[] { "Huh.", "It seems that the number you inputted was not a number.", "4" }
+                        ); return;
                     }
                 }
             );
