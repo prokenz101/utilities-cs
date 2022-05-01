3e613e20cf8ad9532787ace6ef0bf8ab01e2ceda
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-05-01 18:10:30 +0400

    moved hexadecimal back to main command file

--- UtilsCommands.cs
+++ UtilsCommands.cs
@@ -1283,7 +1283,86 @@ namespace utilities_cs {
 
             FormattableCommand hexadecimal = new(
                 commandName: "hexadecimal",
-                function: Hex.HexadecimalMain,
+                function: (string[] args, bool copy, bool notif) => {
+                    string text = string.Join(" ", args[1..]);
+                    if (Utils.IndexTest(args)) {
+                        return null;
+                    }
+
+                    Func<string, string> fromTextToHex = (string text) => {
+                        byte[] ba = System.Text.Encoding.Default.GetBytes(text);
+                        var hexString = BitConverter.ToString(ba);
+                        hexString = hexString.Replace("-", " ");
+                        hexString = hexString.ToLower();
+
+                        return hexString;
+                    };
+
+                    Func<string, byte[]> fromHexToText = (string hex) => {
+                        hex = hex.Replace("-", "");
+                        byte[] raw = new byte[hex.Length / 2];
+                        for (int i = 0; i < raw.Length; i++) {
+                            raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+                        }
+
+                        return raw;
+                    };
+
+                    Func<IEnumerable<char>, bool> isHex = (IEnumerable<char> chars) => {
+                        bool isHex;
+                        foreach (var c in chars) {
+                            isHex = ((c >= '0' && c <= '9') ||
+                                     (c >= 'a' && c <= 'f') ||
+                                     (c >= 'A' && c <= 'F'));
+
+                            if (!isHex)
+                                return false;
+                        }
+
+                        return true;
+                    };
+
+                    string[] textList = text.Split(" ");
+                    string hexWithDash = string.Join("-", textList);
+
+                    if (isHex(string.Join("", args[1..]))) {
+                        try {
+                            string textFromHex = System.Text.Encoding.ASCII.GetString(fromHexToText(hexWithDash));
+                            Utils.CopyCheck(copy, textFromHex);
+                            Utils.NotifCheck(
+                                notif, new string[] { "Success!", $"The message was: {textFromHex}", "10" }, "hexSuccess"
+                            ); return textFromHex;
+                        } catch {
+                            Utils.NotifCheck(
+                                true,
+                                new string[] {
+                                    "Something went wrong.",
+                                    "An exception occured when trying to convert your text from hexadecimal.",
+                                    "4"
+                                },
+                                "hexadecimalError"
+                            ); return null;
+                        }
+                    } else {
+                        try {
+                            string hexFromText = fromTextToHex(text);
+                            Utils.CopyCheck(copy, hexFromText);
+                            Utils.NotifCheck(
+                                notif, new string[] { "Success!", $"Message copied to clipboard.", "3" }, "hexSuccess"
+                            ); return hexFromText;
+                        } catch {
+                            Utils.NotifCheck(
+                                true,
+                                new string[] {
+                                    "Something went wrong.",
+                                    "An exception occured when trying to convert your text into hexadecimal.",
+                                    "4"
+                                },
+                                "hexadecimalError"
+                            ); return null;
+                        }
+                    }
+                },
                 aliases: new string[] { "hex" },
                 useInAllCommand: true,
                 allCommandMode: "encodings"
