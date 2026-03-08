272c1c3635e522e89ccb9f45612752c43f7a581e
prokenz101 <kenz.v@outlook.com>
2026-03-08 11:14:34 +0400

    format now recursively interpolates expressions

--- Dependencies/Format.cs
+++ Dependencies/Format.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using System.Text;
 
 namespace utilities_cs {
     public static partial class Format {
@@ -11,55 +12,121 @@ namespace utilities_cs {
 
         public static void FormatMain(string[] args) {
             if (Utils.IndexTest(args)) { return; }
-            string text = string.Join(" ", args[1..]);
 
-            Dictionary<string, string> formatDict = new();
-            var re = CommandRegex();
-
-            System.Text.RegularExpressions.MatchCollection matches = re.Matches(text);
-
-            foreach (System.Text.RegularExpressions.Match? i in matches) {
-                if (i != null) {
-                    System.Text.RegularExpressions.GroupCollection groups = i.Groups;
-                    System.Text.RegularExpressions.Group mainGroup = groups["command"];
-
-                    string cmd = mainGroup.ToString();
-                    string[] splitcommand = cmd.Split(" ");
-
-                    string? output = FormattableCommand.FindAndExecute(
-                        splitcommand[0],
-                        splitcommand, false, false
-                    );
-
-                    if (output == null) {
-                        output = "errored";
-                        Utils.NotifCheck(
-                            true,
-                            ["Exception", "Invalid command, try 'help' for more info.", "4"],
-                            "formatError"
-                        ); formatDict[cmd] = output;
-                    } else { formatDict[cmd] = output; }
-                }
+            string? result = FindAndReplace(string.Join(" ", args[1..]));
+            if (result is not null) {
+                Utils.CopyCheck(true, result);
+                Utils.NotifCheck(true, ["Success!", "Message copied to clipboard.", "3"], "formatSuccess");
             }
-
-            Utils.CopyCheck(true, ReplaceKeyInString(formatDict, text));
-            Utils.NotifCheck(true, ["Success!", "Message copied to clipboard.", "3"], "formatSuccess");
         }
 
-        static string ReplaceKeyInString(Dictionary<string, string> dictionary, string inputString) {
-            Regex re = ReplaceRegex();
-            var matches = re.Matches(inputString);
-            foreach (System.Text.RegularExpressions.Match? match in matches) {
-                if (match != null) {
-                    var valueWithoutBrackets = match.Groups[1].Value;
-                    var valueWithBrackets = match.Value;
+        static string? FindAndReplace(string? input) {
+            ArgumentNullException.ThrowIfNull(input);
 
-                    if (dictionary.TryGetValue(valueWithoutBrackets, out var replacementValue))
-                        inputString = inputString.Replace(valueWithBrackets, replacementValue);
+            while (true) {
+                int openIndex = -1;
+                for (int i = input.Length - 1; i >= 0; i--) {
+                    if (input[i] != '{') {
+                        continue;
+                    }
+
+                    int slashCount = 0;
+                    for (int j = i - 1; j >= 0 && input[j] == '\\'; j--) {
+                        slashCount++;
+                    }
+
+                    if (slashCount % 2 == 0) {
+                        openIndex = i;
+                        break;
+                    }
+                }
+
+                if (openIndex == -1) {
+                    break;
+                }
+
+                int closeIndex = -1;
+                for (int i = openIndex; i < input.Length; i++) {
+                    if (input[i] != '}') {
+                        continue;
+                    }
+
+                    int slashCount = 0;
+                    for (int j = i - 1; j >= 0 && input[j] == '\\'; j--) {
+                        slashCount++;
+                    }
+
+                    if (slashCount % 2 == 0) {
+                        closeIndex = i;
+                        break;
+                    }
+                }
+
+                if (closeIndex == -1) {
+                    Utils.NotifCheck(true, ["Exception", "Unmatched opening braces.", "3"], "formatError");
+                    return null;
+                }
+
+                string inner = input.Substring(openIndex + 1, closeIndex - openIndex - 1);
+                string[] commandParts = inner.Split(" ");
+                string replacement = FormattableCommand.FindAndExecute(commandParts[0], commandParts, false, false) ?? "errored";
+                if (replacement == "errored") {
+                    Utils.NotifCheck(true, ["Exception", $"Error executing command.", "3"], "formatError");
+                    return null;
+                }
+
+                input = input[..openIndex] + replacement + input[(closeIndex + 1)..];
+            }
+
+            for (int i = input.Length - 1; i >= 0; i--) {
+                if (input[i] != '}') {
+                    continue;
+                }
+
+                int slashCount = 0;
+                for (int j = i - 1; j >= 0 && input[j] == '\\'; j--) {
+                    slashCount++;
+                }
+
+                if (slashCount % 2 == 0) {
+                    Utils.NotifCheck(true, ["Exception", "Unmatched closing braces.", "3"], "formatError");
+                    return null;
                 }
             }
 
-            return inputString;
+            var output = new StringBuilder(input.Length);
+            int index = 0;
+
+            while (index < input.Length) {
+                if (input[index] != '\\') {
+                    output.Append(input[index]);
+                    index++;
+                    continue;
+                }
+
+                int slashStart = index;
+                while (index < input.Length && input[index] == '\\') {
+                    index++;
+                }
+
+                int slashCount = index - slashStart;
+                bool nextIsBrace = index < input.Length && (input[index] == '{' || input[index] == '}');
+
+                if (nextIsBrace && slashCount % 2 == 1) {
+                    output.Append('\\', slashCount - 1);
+                    output.Append(input[index]);
+                    index++;
+                    continue;
+                }
+
+                output.Append('\\', slashCount);
+                if (index < input.Length) {
+                    output.Append(input[index]);
+                    index++;
+                }
+            }
+
+            return output.ToString();
         }
     }
 }
